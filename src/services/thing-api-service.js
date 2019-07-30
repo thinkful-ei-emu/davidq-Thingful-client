a8723e35815316307634e0d885dc7c22ada7d280
David Queen <dsqjr@msn.com>
2019-07-30 14:40:52 -0400

    init

---
+++ src/services/thing-api-service.js
@@ -0,0 +1,61 @@
+import config from '../config'
+import ts from '../services/token-service';
+
+const ThingApiService = {
+  getThings() {
+    return fetch(`${config.API_ENDPOINT}/things`, {
+      headers: {
+      },
+    })
+      .then(res =>
+        (!res.ok)
+          ? res.json().then(e => Promise.reject(e))
+          : res.json()
+      )
+  },
+  getThing(thingId) {
+    return fetch(`${config.API_ENDPOINT}/things/${thingId}`, {
+      headers: {
+        'Authorization':`basic ${ts.hasAuthToken() ? ts.getAuthToken() : 'none'}`
+      },
+    })
+      .then(res =>
+        (!res.ok)
+          ? res.json().then(e => Promise.reject(e))
+          : res.json()
+      )
+  },
+  getThingReviews(thingId) {
+    return fetch(`${config.API_ENDPOINT}/things/${thingId}/reviews`, {
+      headers: {
+        'Authorization':`basic ${ts.hasAuthToken() ? ts.getAuthToken() : 'none'}`
+      },
+    })
+      .then(res =>
+        (!res.ok)
+          ? res.json().then(e => Promise.reject(e))
+          : res.json()
+      )
+  },
+  postReview(thingId, text, rating) {
+    return fetch(`${config.API_ENDPOINT}/reviews`, {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+        'Authorization':`basic ${ts.hasAuthToken() ? ts.getAuthToken() : 'none'}`
+      },
+      body: JSON.stringify({
+        thing_id: thingId,
+        rating,
+        text,
+      }),
+    })
+      .then(res =>
+        (!res.ok)
+          ? res.json().then(e => Promise.reject(e))
+          : res.json()
+      )
+  }
+}
+
+export default ThingApiService
